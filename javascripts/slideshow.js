6628e8e635d7b59310fb4c44ae0c1d6a8df48494
Matthew Garrett <mattga@gmail.com>
2008-09-04 00:52:48 -0500

    Sketching out the basic demo.

--- javascripts/slideshow.js
+++ javascripts/slideshow.js
@@ -19,8 +19,8 @@ var SlideShow = Class.create({
     this.root = $(element);
     this.fireEvent('initializing', { slideshow: this });
     this.defaultOptions = $H({
-      autoStart: true,
-      duration: 5,
+      autoPlay: true,
+      slideDuration: 5,
       transitionDuration: 1,
       loop: true,
       overlap: false,
@@ -32,54 +32,81 @@ var SlideShow = Class.create({
     this.effectOptions = { duration: this.options.get('transitionDuration') / 2 };
     this.slides = this.options.get('slides');
     this.prep();
-    if (this.options.get('autoStart')) this.start();
+    this.paused = false;
+    if (this.options.get('autoPlay')) this.play();
     this.fireEvent('initialized', { slideshow: this });
   },
   prep: function(){
     this.root.makePositioned();
     this.slides
-      .invoke('setStyle', { display: 'none', position: 'absolute' })
+      .invoke('setStyle', { display: 'none', position: 'absolute', opacity: 0 })
       .each(function(slide, i){ slide.setStyle({ zIndex: i }); });
     this.fireEvent('prepped', { slideshow: this });
   },
-  start: function(){
+  play: function(){
+    this.paused = false;
     this.transition();
     this.fireEvent('started', { slideshow: this });
   },
+  pause: function(){
+    this.paused = true;
+  },
   transition: function(){
+    if (this.paused) return;
     // get slide after visible one, or 1st one if last is visible or none are visible
-    this.coming = this.slides.find(function(slide){
-      if (slide.visible()) return slide.next() || this.slides.first();
-    }.bind(this)) || this.slides.first();
-    // console.log(this.coming);
-    this.going = this.coming.previous() || this.slides.first();
+    var coming = this.slides.find(function(slide){ return slide.visible(); });
+    this.coming = (coming) ? coming.next() : this.slides.first();
     
+    this.going = this.coming.previous() || this.slides.first();
     // if not fresh start, fade
     if (this.going != this.coming) {
+      // this.coming.show();
       // if overlap
       if (this.options.get('overlap')) {
-        new Effect.Parallel([new Effect.Appear(this.coming), new Effect.Fade(this.going)], { duration: this.options.get('transitionDuration') });
+        new Effect.Parallel(
+          [new Effect.Appear(this.coming.show()), new Effect.Fade(this.going)],
+          {
+            duration: this.options.get('transitionDuration'),
+            afterFinish: Element.hide.curry(this.going)
+          }
+        );
+        console.group('transition: ' + this.loopCount);
+        console.log('coming: ' + this.coming.down().readAttribute('alt'));
+        console.log('going: ' + this.going.down().readAttribute('alt'));
+        console.groupEnd();
       } else {
-        going.fade(this.effectOptions.merge({
+        this.going.fade(this.effectOptions.merge({
           afterFinish: function(){
-            this.coming.appear(this.effectOptions);
+            console.log('this.going is faded out');
+            this.going.hide();
+            this.coming.show().appear({afterFinish: function(){
+              console.log(this.coming);
+            }});
           }.bind(this)
         }));
+        console.group('transition: ' + this.loopCount);
+        console.log('coming: ' + this.coming.down().readAttribute('alt'));
+        console.log('going: ' + this.going.down().readAttribute('alt'));
+        console.groupEnd();
       }
     }
     // fade in the first time
     else {
+      console.log('fade in the first time');
+      console.group('transition: ' + this.loopCount);
+      console.log('coming: ' + this.coming.down().readAttribute('alt'));
+      console.log('going: ' + this.going.down().readAttribute('alt'));
+      console.groupEnd();
+      
       this.coming.appear(this.effectOptions);
     }
     
-    
     this.loopCount++;
-    this.fireEvent('transitioned', { slideshow: this, coming: this.coming, going: this.going });
+    this.fireEvent('transitioned', { slideshow: this, coming: this.coming, going: this.going, loopCount: this.loopCount });
+    if (this.options.get('slideDuration') > 0)
+      this.transition.bind(this).delay(this.options.get('slideDuration'));
   },
   fireEvent: function(name, memo){
-    // console.group(name);
-    // console.log(memo);
-    // console.groupEnd();
     this.root.fire(this.root.identify() + '_slideshow:' + name, memo);
   }
 });
